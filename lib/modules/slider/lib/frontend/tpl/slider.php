e14498c0bf1b2ccf93c3deb7cb4c39665fe5c5ed
Dennis Heiden <heiden@straightvisions.com>
2022-08-16 14:48:47 +0200

    add swiffy to frontend

---
+++ lib/modules/slider/lib/frontend/tpl/slider.php
@@ -0,0 +1,10 @@
+<?php
+	$align = $attr['align'] ? 'align'.$attr['align'] : '';
+	var_dump($attr);
+?>
+<div class="sv-slider-wrapper <?php echo $align;?>">
+
+</div>
+
+
+
--- lib/modules/slider/slider.php
+++ lib/modules/slider/slider.php
@@ -14,7 +14,7 @@
 				->load_settings()
 				->get_root()->add_section( $this );
 			
-			add_action( 'init', array($this, 'register_block') );
+			//add_action( 'init', array($this, 'register_block') );
 			add_action( 'init', array($this, 'register_scripts') );
 
 		}
@@ -29,11 +29,19 @@
 		}
 		
 		public function register_block(): slider{
-			$path = $this->get_path('lib/backend/src/block.json') ;
+			$path = $this->get_path('lib/backend/src/components/wrapper/block.json') ;
 			
 			if ( is_string( $path ) && file_exists( $path ) ) {
 				register_block_type_from_metadata( $path,
-					array('render_callback' => array($this, 'render_block'))
+					array('render_callback' => array($this, 'render_block_wrapper'))
+				);
+			}
+			
+			$path = $this->get_path('lib/backend/src/components/slide/block.json') ;
+			
+			if ( is_string( $path ) && file_exists( $path ) ) {
+				register_block_type_from_metadata( $path,
+					array('render_callback' => array($this, 'render_block_slide'))
 				);
 			}
 			
@@ -41,7 +49,6 @@
 		}
 		
 		public function register_scripts(): slider {
-			
 			if(is_admin()){
 				
 				$this->get_script('sv_slider_editor_script')
@@ -51,49 +58,23 @@
 				     ->set_is_backend()
 				     ->set_is_enqueued();
 
-				/*
-				$this->get_script('sv_slider_editor_css')
-				     ->set_path('lib/backend/css/xxxx.css')
-				     ->set_is_gutenberg()
-				     ->set_is_backend()
-				     ->set_is_enqueued();*/
-			}
-			
-			return $this;
-		}
-		
-		public function render_block( array $attr, string $content ): string {
-			
-			$this->load_frontend_scripts($attr);
-			
-			ob_start();
-			if($attr['player'] === 'iframe'){
-				//require( $this->get_path( 'lib/frontend/tpl/block_iframe.php' ) );
-			}
-			if($attr['player'] === 'html5'){
-				//require( $this->get_path( 'lib/frontend/tpl/block_html5.php' ) );
-			}
-			
-			$output = ob_get_clean();
-			
-			return $output;
-		}
-		
-		private function load_frontend_scripts($attr): slider{
-			/* $this->get_script( 'sv_cloudflare_stream_frontend_css' )
-			     ->set_path( 'lib/frontend/css/player.css' )
-			     ->set_is_enqueued();*/
-		
-			if ( $attr['player'] === 'iframe' ) {
-				/*
-				$this->get_script( 'sv_cloudflare_stream_frontend_iframe_player_vendor' )
-				     ->set_path( 'lib/frontend/js/sdk.latest.js' )
-				     ->set_type( 'js' )
+			}else{
+				$this->get_script('sv_slider_swiffy_slider_css')
+				     ->set_path('lib/vendor/swiffy_slider/dist/css/swiffy-slider.min.css')
 				     ->set_is_enqueued();
-				*/
+				
+				$this->get_script('sv_slider_swiffy_slider_js')
+				     ->set_path('lib/vendor/swiffy_slider/dist/js/swiffy-slider.js')
+				     ->set_type('js')
+				     ->set_is_enqueued();
+				
+				$this->get_script('sv_slider_js')
+				     ->set_path('lib/frontend/js/slider.js')
+				     ->set_type('js')
+				     ->set_is_enqueued();
 			}
 			
 			return $this;
 		}
-		
+	
 	}
