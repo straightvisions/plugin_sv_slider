8de34e548b231481c34f55832de70bd6e70efb1c
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-01-26 14:23:24 +0100

    BAAR-61 - fix hidden indicator output

--- lib/modules/slider/lib/frontend/tpl/slider.php
+++ lib/modules/slider/lib/frontend/tpl/slider.php
@@ -36,7 +36,7 @@ $classnames = implode(' ', array_filter([
 <button type="button" class="slider-nav" aria-label="Go to previous"></button>
 <button type="button" class="slider-nav slider-nav-next" aria-label="Go to next"></button>
 
-<?php if(1===1){
+<?php if($attributes['svSlider']['indicators-style'] !== 'none'){
 	$indicators = '<ul class="slider-indicators">';
 
 	$count = isset($attributes['svSlider']['childrenCount']) && (int)$attributes['svSlider']['childrenCount'] > 0 ? (int)$attributes['svSlider']['childrenCount'] : 1;
