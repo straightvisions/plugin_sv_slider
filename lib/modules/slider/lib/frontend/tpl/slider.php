0dda00c1b5950e3f759c3a800b997b77c4b31b8c
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-01-26 13:12:37 +0100

    BAAR-161 - add slider dot indicators as default (locked)

--- lib/modules/slider/lib/frontend/tpl/slider.php
+++ lib/modules/slider/lib/frontend/tpl/slider.php
@@ -1,5 +1,5 @@
 <<?php
-echo $tag; echo $id;?> class="wp-block-straightvisions-sv-slider swiffy-slider slider-nav-visible<?php
+echo $tag; echo $id;?> class="wp-block-straightvisions-sv-slider slider-indicators-round swiffy-slider slider-nav-visible<?php
 echo empty($attributes['className']) ? '' : ' '.$attributes['className']; ?> align<?php
 echo isset($attributes['align']) ? $attributes['align'] : ''; ?> <?php
 echo $this->class_selector; ?> " <?php
@@ -12,6 +12,20 @@ echo $attributes['_data']; ?>>
 
 <button type="button" class="slider-nav" aria-label="Go to previous"></button>
 <button type="button" class="slider-nav slider-nav-next" aria-label="Go to next"></button>
+
+<?php if(1===1){
+	$indicators = '<ul class="slider-indicators">';
+
+	$count = isset($attributes['svSlider']['childrenCount']) && (int)$attributes['svSlider']['childrenCount'] > 0 ? (int)$attributes['svSlider']['childrenCount'] : 1;
+
+	for($i = 1; $i <= $count; $i++){
+        $indicators .= '<li></li>';
+	}
+
+    $indicators .= '</ul>';
+
+	echo $indicators;
+} ?>
 
 </<?php
 echo $tag ?>>
